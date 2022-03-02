6b12e163bb8febed5e01349bc9aa2eb055931ad7
Eric de Sousa <ericdesousa@outlook.com>
2022-03-02 18:00:34 +0000

    finished adding values to all axis of radar

--- src/tcrcloud/radar.py
+++ src/tcrcloud/radar.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import skbio
 import airr
 
+plt.rcParams["font.family"] = "serif"
+
 def convergence(df,length):
     counts=df['counts']
     total=counts.sum()
@@ -81,10 +83,10 @@ def format_data(args):
 def radar(args):
     categories = [
                     'D50\nIndex',
+                    'Chao1\nIndex',
                     'Convergence', 
                     'Shannon\nIndex', 
                     'Simpson\nIndex', 
-                    'Chao1\nIndex',
                     'Gini\nindex',
                     'Distinct\nCDR3',]
     categories = [*categories, categories[0]]
@@ -105,23 +107,23 @@ def radar(args):
         convergencelist= np.array([0,convergence(df,length),1]).reshape(-1, 1) 
         shannon= np.array([0,skbio.diversity.alpha_diversity('shannon',counts)[0],15]).reshape(-1, 1)
         simpson= np.array([0,skbio.diversity.alpha_diversity('simpson',counts)[0],1]).reshape(-1, 1)
-        chao= np.array([0,skbio.diversity.alpha_diversity('chao1',counts)[0],70000]).reshape(-1, 1)
+        chao= np.array([0,skbio.diversity.alpha_diversity('chao1',counts)[0],120000]).reshape(-1, 1)
         gini= np.array([0,skbio.diversity.alpha_diversity('gini_index',counts)[0],1]).reshape(-1, 1)
         dfiftylist= np.array([0,Dfifty(df,length),1]).reshape(-1, 1)
         metrics=[]
         metrics.append(j[1]+' '+j[0])
         metrics.append(minmax_scale.fit_transform(dfiftylist)[1].astype(np.float))
+        metrics.append(minmax_scale.fit_transform(chao)[1].astype(np.float))
         metrics.append(minmax_scale.fit_transform(convergencelist)[1].astype(np.float))
         metrics.append(minmax_scale.fit_transform(shannon)[1].astype(np.float))
         metrics.append(minmax_scale.fit_transform(simpson)[1].astype(np.float))
-        metrics.append(minmax_scale.fit_transform(chao)[1].astype(np.float))
         metrics.append(minmax_scale.fit_transform(gini)[1].astype(np.float))
         metrics.append(minmax_scale.fit_transform(distinct)[1].astype(np.float))
         patients.append(metrics)
     
     label_loc = np.linspace(start=0, stop=2 * np.pi, num=len(patients[0]))
 
-    plt.figure(figsize=(12.5,8))
+    plt.figure(figsize=(10,12))
     plt.subplot(polar=True)
     len_label=0
     radar_colours=['#CC6677', '#332288', '#DDCC77', '#117733', '#88CCEE',
@@ -138,34 +140,61 @@ def radar(args):
         plt.fill(label_loc, patient, linewidth=4.0, alpha=0.6,color=thecolour)
         if len(i[0]) > len_label:
             len_label = len(i[0])
-    plt.title('Repertoire profile\ncomparison', size=20, y=1.20)
+    plt.title('Repertoire profile\ncomparison', size=20, y=1.10)
 
-    plt.text(label_loc[0], 0.06,'5',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[0], 0.26,'15',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[0], 0.46,'25',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[0], 0.66,'35',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[0], 0.86,'45',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[0], 1.03,'50',horizontalalignment='center',verticalalignment='center')
+    plt.text(label_loc[0], 0.06,'5',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[0], 0.26,'15',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[0], 0.46,'25',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[0], 0.66,'35',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[0], 0.86,'45',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[0], 1.03,'50',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
     
-    plt.text(label_loc[1], 0.06,'0.1',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[1], 0.26,'0.3',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[1], 0.46,'0.5',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[1], 0.66,'0.7',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[1], 0.86,'0.9',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[1], 1.03,'1',horizontalalignment='center',verticalalignment='center')
+    plt.text(label_loc[1], 0.06,'11000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[1], 0.26,'33000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[1], 0.46,'55000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[1], 0.66,'77000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[1], 0.86,'99000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[1], 1.03,'110000',horizontalalignment='left',verticalalignment='center',fontsize=12,fontweight='bold')
     
-    plt.text(label_loc[6], 0.06,'1000',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[6], 0.26,'3000',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[6], 0.46,'5000',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[6], 0.66,'7000',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[6], 0.86,'9000',horizontalalignment='center',verticalalignment='center')
-    plt.text(label_loc[6], 1.03,'10000',horizontalalignment='center',verticalalignment='center')
-
+    plt.text(label_loc[2], 0.06,'0.1',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[2], 0.26,'0.3',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[2], 0.46,'0.5',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[2], 0.66,'0.7',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[2], 0.86,'0.9',horizontalalignment='center',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[2], 1.03,'1',horizontalalignment='center',verticalalignment='bottom',fontsize=12,fontweight='bold')
+    
+    plt.text(label_loc[3], 0.06,'1.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[3], 0.26,'4.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[3], 0.46,'7.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[3], 0.66,'10.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[3], 0.86,'13.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[3], 1.03,'15',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    
+    plt.text(label_loc[4], 0.06,'0.1',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[4], 0.26,'0.3',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[4], 0.46,'0.5',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[4], 0.66,'0.7',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[4], 0.86,'0.9',horizontalalignment='right',verticalalignment='center',fontsize=12,fontweight='bold')
+    plt.text(label_loc[4], 1.03,'1',horizontalalignment='right',verticalalignment='top',fontsize=12,fontweight='bold')
+    
+    plt.text(label_loc[5], 0.06,'0.1',horizontalalignment='center',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[5], 0.26,'0.3',horizontalalignment='center',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[5], 0.46,'0.5',horizontalalignment='center',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[5], 0.66,'0.7',horizontalalignment='center',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[5], 0.86,'0.9',horizontalalignment='center',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[5], 1.03,'1',horizontalalignment='center',verticalalignment='top', fontsize=12,fontweight='bold')
+    
+    plt.text(label_loc[6], 0.06,'1000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[6], 0.26,'3000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[6], 0.46,'5000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[6], 0.66,'7000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[6], 0.86,'9000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    plt.text(label_loc[6], 1.03,'10000',horizontalalignment='left',verticalalignment='center', fontsize=12,fontweight='bold')
+    
     plt.yticks([0.1, 0.3, 0.5, 0.7, 0.9],[])
-    plt.tick_params(pad=22,labelsize=16)
+    plt.tick_params(pad=30,labelsize=16)
     lines, labels = plt.thetagrids(np.degrees(label_loc), labels=categories)
-    horizontallegend=1.2+(len_label*.025)
     outputname = args.rearrangements[:-4]+"_radar"+".png"
-    plt.legend(loc='upper right',bbox_to_anchor=(horizontallegend, 1.2),fontsize=16)
+    plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2),fontsize=16)
     plt.tight_layout()
     plt.savefig(outputname,dpi=300)
