cce69dd45fbd7c08b4bbc75e5c4f4dac86bf966f
Eric de Sousa <ericdesousa@outlook.com>
2022-03-25 14:19:03 +0000

    added better error handling when downloading files

--- src/tcrcloud/TCRcloud.py
+++ src/tcrcloud/TCRcloud.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import argparse
+import yaml
+import json
 
 import matplotlib.pyplot as plt
 
@@ -14,72 +16,83 @@ plt.rcParams["font.family"] = "serif"
 def main():
     # create the top-level parser
     parser = argparse.ArgumentParser(
-        description='Create a wordcloud of CDR3 \
-    sequences from TCR AIRR-seq data or a radar plot with diversity metrics.',
-        prog='TCRcloud')
+        description="Create a wordcloud of CDR3 \
+    sequences from TCR AIRR-seq data or a radar plot with diversity metrics.",
+        prog="TCRcloud")
 
     subparsers = parser.add_subparsers(
-        title='Command Options',
-        help='The program has 3 modes: cloud, radar or download',
-        dest='cloud, radar, download, testdata or -h/--help',
+        title="Command Options",
+        help="The program has 3 modes: cloud, radar or download",
+        dest="cloud, radar, download, testdata or -h/--help",
         required=True)
 
     # create subparser for making the wordcloud
-    parser_cloud = subparsers.add_parser('cloud', help='Create a wordcloud \
-        from TCR CDR3 data')
+    parser_cloud = subparsers.add_parser("cloud", help="Create a wordcloud \
+        from TCR CDR3 data")
 
-    required_group = parser_cloud.add_argument_group('required arguments')
+    required_group = parser_cloud.add_argument_group("required arguments")
 
-    # required_group.add_argument('-r','--repertoire',
+    # required_group.add_argument("-r","--repertoire",
     #     type= tcrcloud.base.jsonfile,
-    #     help= 'indicate the name of the AIRR Standards repertoire file',
-    #     metavar= 'repertoires.airr.json',
+    #     help= "indicate the name of the AIRR Standards repertoire file",
+    #     metavar= "repertoires.airr.json",
     #     required= True)
 
-    required_group.add_argument('-a', '--rearrangements', type=str,
-                                help='indicate the name of the AIRR Standards \
-                                rearrangements file',
-                                metavar='rearrangements.tsv', required=True)
+    required_group.add_argument("-a", "--rearrangements", type=str,
+                                help="indicate the name of the AIRR Standards \
+                                rearrangements file",
+                                metavar="rearrangements.tsv", required=True)
 
     parser_cloud.set_defaults(func=tcrcloud.cloud.wordcloud)
 
     # create subparser for making the radar
-    parser_radar = subparsers.add_parser('radar', help='Create a radar plot \
-        with diversity metrics')
-    required_group = parser_radar.add_argument_group('required arguments')
-    required_group.add_argument('-a', '--rearrangements', type=str,
-                                help='indicate the name of the AIRR \
-                                Standards rearrangements file',
-                                metavar='rearrangements.tsv', required=True)
+    parser_radar = subparsers.add_parser("radar", help="Create a radar plot \
+        with diversity metrics")
+    required_group = parser_radar.add_argument_group("required arguments")
+    required_group.add_argument("-a", "--rearrangements", type=str,
+                                help="indicate the name of the AIRR \
+                                Standards rearrangements file",
+                                metavar="rearrangements.tsv", required=True)
     parser_radar.set_defaults(func=tcrcloud.radar.radar)
 
     # create subparser for downloading the rearregement data
-    parser_download = subparsers.add_parser('download',
-                                            help='Download TCR AIRR-seq \
+    parser_download = subparsers.add_parser("download",
+                                            help="Download TCR AIRR-seq \
                                             rearrangements data matching a \
-                                            given repertoire metadata file')
+                                            given repertoire metadata file")
 
-    required_group = parser_download.add_argument_group('required arguments')
+    required_group = parser_download.add_argument_group("required arguments")
 
-    required_group.add_argument('-r', '--repertoire', type=str,
-                                help='indicate the name of the \
-                                AIRR Standards repertoire file',
-                                metavar='repertoires.airr.json',
+    required_group.add_argument("-r", "--repertoire", type=str,
+                                help="indicate the name of the \
+                                AIRR Standards repertoire file",
+                                metavar="repertoires.airr.json",
                                 required=True)
 
     parser_download.set_defaults(func=tcrcloud.download.airrdownload)
 
     # create subparser for downloading the test repertoire
 
-    parser_testdata = subparsers.add_parser('testdata',
-                                            help='Download TCR AIRR-seq \
-                                            repertoire file to test TCRcloud')
+    parser_testdata = subparsers.add_parser("testdata",
+                                            help="Download TCR AIRR-seq \
+                                            repertoire file to test TCRcloud")
 
     parser_testdata.set_defaults(func=tcrcloud.testdata.download)
 
     args = parser.parse_args()
-    args.func(args)
+    try:
+        args.func(args)
+    except FileNotFoundError:
+        if dir(args)[-1] == "repertoire":
+            print("TCRcloud error: " + args.repertoire
+                  + " doesn't seem to exist")
+        elif dir(args)[-1] == 'rearrangements':
+            print("TCRcloud error: " + args.rearrangements
+                  + " doesn't seem to exist")
+    except (yaml.scanner.ScannerError, json.decoder.JSONDecodeError):
+        print("TCRcloud error: It seems you did not indicate a properly \
+formatted AIRR repertoire file")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/tcrcloud/download.py
+++ src/tcrcloud/download.py
@@ -1,55 +1,58 @@
 import requests
-
 import airr
 
 
 def testserver(data):
     # Find which server has the matching rearragements
     query = {
-        'filters': {
-            'op': '=',
-            'content': {
-                'field': 'repertoire_id',
-                'value': data['Repertoire'][0]['repertoire_id']
+        "filters": {
+            "op": "=",
+            "content": {
+                "field": "repertoire_id",
+                "value": data["Repertoire"][0]["repertoire_id"]
             }
         }
     }
-    repositories = ['https://vdjserver.org/airr/v1',
-                    'https://ipa1.ireceptor.org/airr/v1',
-                    'https://ipa2.ireceptor.org/airr/v1',
-                    'https://ipa3.ireceptor.org/airr/v1',
-                    'https://ipa4.ireceptor.org/airr/v1',
-                    'http://ipa5.ireceptor.org/airr/v1',
-                    'http://covid19-1.ireceptor.org/airr/v1',
-                    'http://covid19-2.ireceptor.org/airr/v1',
-                    'http://covid19-3.ireceptor.org/airr/v1',
-                    'http://covid19-4.ireceptor.org/airr/v1',
-                    'https://scireptor.dkfz.de/airr/v1',
-                    'http://airr-seq.vdjbase.org/airr/v1']
+    repositories = ["https://vdjserver.org/airr/v1",
+                    "https://ipa1.ireceptor.org/airr/v1",
+                    "https://ipa2.ireceptor.org/airr/v1",
+                    "https://ipa3.ireceptor.org/airr/v1",
+                    "https://ipa4.ireceptor.org/airr/v1",
+                    "http://ipa5.ireceptor.org/airr/v1",
+                    "http://covid19-1.ireceptor.org/airr/v1",
+                    "http://covid19-2.ireceptor.org/airr/v1",
+                    "http://covid19-3.ireceptor.org/airr/v1",
+                    "http://covid19-4.ireceptor.org/airr/v1",
+                    "https://scireptor.dkfz.de/airr/v1",
+                    "http://airr-seq.vdjbase.org/airr/v1"]
 
-    host_url = 'https://vdjserver.org/airr/v1'
+    host_url = "https://vdjserver.org/airr/v1"
     for i in repositories:
         test_url = i
-        resp = requests.post(test_url + '/rearrangement', json=query)
-        if len(resp.json()['Rearrangement']) > 0:
+        resp = requests.post(test_url + "/rearrangement", json=query)
+        if len(resp.json()["Rearrangement"]) > 0:
             host_url = i
     return host_url
 
 
 def airrdownload(args):
-    validate = airr.validate_repertoire(args.repertoire)
+    try:
+        airr.validate_repertoire(args.repertoire)
+    except TypeError:
+        print("TCRcloud error: unknown file type for repertoire. Supported \
+file extensions are 'yaml', 'yml' or 'json'")
     repertoire_file = args.repertoire
-    rearrangements_file = repertoire_file[:-4] + 'rearrangements.tsv'
+    rearrangements_file = repertoire_file[:-4] + "rearrangements.tsv"
     data = airr.load_repertoire(args.repertoire)
-    repertoires = data['Repertoire']
+    repertoires = data["Repertoire"]
     host_url = testserver(data)
 
     # Print out some Info
-    print('       Info: ' + data['Info']['title'])
-    print('    version: ' + str(data['Info']['version']))
-    print('description: ' + data['Info']['description'])
-    print('Found ' + str(len(data['Repertoire'])) + ' repertoires in \
-metadata file.')
+    print("       Info: " + data["Info"]["title"])
+    print("    version: " + str(data["Info"]["version"]))
+    print("description: " + data["Info"]["description"])
+    print("Found " + str(len(data["Repertoire"])) + " repertoires in \
+repertoire metadata file.")
 
     # Query the rearrangement endpoint
     # Define a generic query object, and we will replace the repertoire_id
@@ -57,27 +60,27 @@ metadata file.')
     # an additional filter.
 
     query = {
-        'filters': {
-            'op': 'and',
-            'content': [
+        "filters": {
+            "op": "and",
+            "content": [
                 {
-                    'op': '=',
-                    'content': {
-                        'field': 'repertoire_id',
-                        'value': 'XXX'
+                    "op": "=",
+                    "content": {
+                        "field": "repertoire_id",
+                        "value": "XXX"
                     }
                 },
                 {
-                    'op': '=',
-                    'content': {
-                        'field': 'productive',
-                        'value': True
+                    "op": "=",
+                    "content": {
+                        "field": "productive",
+                        "value": True
                     }
                 }
             ]
         },
-        'size': 1000,
-        'from': 0
+        "size": 1000,
+        "from": 0
     }
 
     # Loop through each repertoire and query rearrangement data for
@@ -86,18 +89,18 @@ metadata file.')
 
     first = True
     for r in repertoires:
-        print('Retrieving rearrangements for repertoire: '
-              + r['repertoire_id'])
-        query['filters']['content'][0]['content']['value'] = r['repertoire_id']
-        query['size'] = 1000
-        query['from'] = 0
+        print("Retrieving rearrangements for repertoire: "
+              + r["repertoire_id"])
+        query["filters"]["content"][0]["content"]["value"] = r["repertoire_id"]
+        query["size"] = 1000
+        query["from"] = 0
 
         cnt = 0
         while True:
             # send the request
-            resp = requests.post(host_url + '/rearrangement', json=query)
+            resp = requests.post(host_url + "/rearrangement", json=query)
             data = resp.json()
-            rearrangements = data['Rearrangement']
+            rearrangements = data["Rearrangement"]
 
             # Open a file for writing the rearrangements. We do this here
             # because we need to know the full set of fields being
@@ -115,12 +118,12 @@ metadata file.')
 
             # looping until zero rearrangements are returned from the query.
             cnt += len(rearrangements)
-            print(cnt)
             if len(rearrangements) < 1000:
                 break
 
             # Need to update the from parameter to get the next chunk
-            query['from'] = cnt
+            query["from"] = cnt
 
-        print('Retrieved ' + str(cnt) + ' rearrangements for repertoire: '
-                           + r['repertoire_id'])
+        print("Retrieved " + str(cnt) + " rearrangements for repertoire: "
+                           + r["repertoire_id"])
+        print("Saved as " + rearrangements_file)
