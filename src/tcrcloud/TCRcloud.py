38f48766eec93c6dba946753e499208224765fb1
Eric de Sousa <ericdesousa@outlook.com>
2022-04-07 10:31:41 +0100

    version 1.0.0

--- src/tcrcloud/TCRcloud.py
+++ src/tcrcloud/TCRcloud.py
@@ -21,7 +21,7 @@ def main():
     sequences from TCR AIRR-seq data or a radar plot with diversity metrics.",
         prog="TCRcloud")
     parser.add_argument('--version', action='version',
-                        version='%(prog)s 0.3.0')
+                        version='%(prog)s 1.0.0')
     subparsers = parser.add_subparsers(
         title="command options",
         help="The program has 4 options: cloud, radar, download or testdata",
