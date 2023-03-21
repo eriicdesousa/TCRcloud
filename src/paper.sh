4e7df836a661aa8e37c5f76080248448f673bb43
Eric de Sousa <ericdesousa@outlook.com>
2023-03-21 18:45:39 +0000

    add script to produce examples

---
+++ src/paper.sh
@@ -0,0 +1,8 @@
+pip3 install TCRcloud
+TCRcloud testdata
+TCRcloud download alpharepertoire.airr.json
+TCRcloud download betarepertoire.airr.json
+TCRcloud cloud alpharepertoire.airr.rearrangements.tsv -l false
+TCRcloud cloud betarepertoire.airr.rearrangements.tsv -l false
+TCRcloud radar alpharepertoire.airr.rearrangements.tsv -l false
+TCRcloud radar betarepertoire.airr.rearrangements.tsv -l false
