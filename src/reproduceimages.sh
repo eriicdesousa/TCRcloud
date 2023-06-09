7e8f88830f5eed1a19d937327f71707972ba8951
Eric de Sousa <ericdesousa@outlook.com>
2023-06-09 15:53:12 +0100

    update script for the paper

--- src/reproduceimages.sh
+++ src/reproduceimages.sh
@@ -4,7 +4,7 @@ TCRcloud download alpharepertoire.airr.json
 TCRcloud download betarepertoire.airr.json
 TCRcloud cloud alpharepertoire.airr.rearrangements.tsv -l false
 TCRcloud cloud betarepertoire.airr.rearrangements.tsv -l false
-TCRcloud radar alpharepertoire.airr.rearrangements.tsv -l false
-TCRcloud radar betarepertoire.airr.rearrangements.tsv -l false
-TCRcloud surface -zt 3.5 -yt 25 -yb 5 alpharepertoire.airr.rearrangements.tsv
-TCRcloud surface -zt 3.5 -yt 28 -yb 8 betarepertoire.airr.rearrangements.tsv
+TCRcloud radar alpharepertoire.airr.rearrangements.tsv -l false -ut 2000 -cht 2000 -ct 0.1
+TCRcloud radar betarepertoire.airr.rearrangements.tsv -l false -ut 2000 -cht 2000 -ct 0.1
+TCRcloud surface alpharepertoire.airr.rearrangements.tsv -zt 3.5 -yt 25 -yb 5 
+TCRcloud surface betarepertoire.airr.rearrangements.tsv -zt 3.5 -yt 28 -yb 8 
