5dd456886488ba79e5f36669c9575aac84f912fa
Eric de Sousa <ericdesousa@outlook.com>
2025-05-02 13:01:53 +0100

    update the nomenclature of some colours

--- src/reproduceimages.sh
+++ src/reproduceimages.sh
@@ -6,8 +6,8 @@ TCRcloud cloud alpharepertoire.airr.rearrangements.tsv
 TCRcloud cloud betarepertoire.airr.rearrangements.tsv
 TCRcloud radar alpharepertoire.airr.rearrangements.tsv -l false -uh 2000 -chh 2000 -ch 0.1
 TCRcloud radar betarepertoire.airr.rearrangements.tsv -l false -uh 2000 -chh 2000 -ch 0.1
-TCRcloud vgenes alpharepertoire.airr.rearrangements.tsv -zla -0.01 -zha 3.5 -yha 23 -yla 5 
-TCRcloud vgenes betarepertoire.airr.rearrangements.tsv -zlb -0.01 -zhb 3.5 -yhb 28
+TCRcloud vgenes alpharepertoire.airr.rearrangements.tsv -zla -0.01 -zha 4 -yha 23 -yla 5 
+TCRcloud vgenes betarepertoire.airr.rearrangements.tsv -zlb -0.01 -zhb 4 -yhb 28
 TCRcloud vgenes alpharepertoire.airr.rearrangements.tsv -c True
 TCRcloud vgenes betarepertoire.airr.rearrangements.tsv -c True
 TCRcloud aminoacids alpharepertoire.airr.rearrangements.tsv -l 21
