e0ac246137c56388a21f91ade013e82cbd4a5cba
Eric de Sousa <ericdesousa@outlook.com>
2026-03-18 19:20:56 +0100

    modify some colours

---
+++ src/tcrcloud/colours_macaca_fascicularis.py
@@ -0,0 +1,106 @@
+"""V-gene colour palette for *Macaca fascicularis*."""
+
+TRAV = {
+  "TRAV1-1": "#1E4FD1",
+  "TRAV1-2": "#2A5BE0",
+  "TRAV2": "#1E88E5",
+  "TRAV3": "#00A4D6",
+  "TRAV4": "#00B3AD",
+  "TRAV5": "#00C29A",
+  "TRAV6": "#00B86A",
+  "TRAV8-1": "#36B24E",
+  "TRAV8-2": "#4FBE63",
+  "TRAV9-1": "#7BCB4A",
+  "TRAV9-2": "#8FD25F",
+  "TRAV10": "#A5D63A",
+  "TRAV11-1": "#B7DE2F",
+  "TRAV11-2": "#BEDB4F",
+  "TRAV11-3": "#C6DD6A",
+  "TRAV12-1": "#D9D12A",
+  "TRAV12-2": "#DFD64F",
+  "TRAV12-3": "#E5DC73",
+  "TRAV13-1": "#E8C23A",
+  "TRAV13-2": "#EBCB63",
+  "TRAV14/DV4": "#E6A93A",
+  "TRAV15": "#E79A2E",
+  "TRAV16": "#EF8A28",
+  "TRAV17": "#F07932",
+  "TRAV18": "#F0653C",
+  "TRAV19": "#ED4E3F",
+  "TRAV20": "#E53935",
+  "TRAV21": "#D62E4E",
+  "TRAV22-1": "#C8326E",
+  "TRAV22-3": "#D24F86",
+  "TRAV23-1": "#B8329B",
+  "TRAV23-3": "#C24FB0",
+  "TRAV24-1": "#A33ACF",
+  "TRAV24-2": "#AD5BDA",
+  "TRAV25-1": "#8F47E0",
+  "TRAV25-2": "#9B63E6",
+  "TRAV26-1": "#6B5EEA",
+  "TRAV26-2": "#7A73EE",
+  "TRAV26-3": "#8A86F1",
+  "TRAV27": "#5868D8",
+  "TRAV29/DV5": "#3B5BD8",
+  "TRAV30": "#2E6DD1",
+  "TRAV31": "#2789D1",
+  "TRAV32": "#209DD1",
+  "TRAV33": "#18AFC4",
+  "TRAV34": "#16B6AA",
+  "TRAV35": "#10C3A0",
+  "TRAV36/DV7": "#0FB98D",
+  "TRAV38-1": "#09B274",
+  "TRAV38-2/DV8": "#11BE7E",
+  "TRAV39": "#14C07B",
+  "TRAV40": "#2DBE63",
+  "TRAV41": "#6AC253",
+}
+
+
+TRBV = {
+    
+}
+
+TRGV = {
+    
+}
+
+TRDV = {
+    
+}
+
+IGHV = {
+    
+}
+
+IGKV = {
+    
+}
+
+
+IGLV = {
+    
+}
+
+aminoacids = {
+    "L": "#003366",
+    "I": "#004080",
+    "V": "#0055A4",
+    "M": "#0072BB",
+    "A": "#0099CC",
+    "P": "#00B3E6",
+    "G": "#00E5F8",
+    "F": "#005500",
+    "W": "#008000",
+    "Y": "#00EE00",
+    "T": "#FF5A1F",
+    "S": "#FF751F",
+    "Q": "#FFA020",
+    "N": "#FFBF1F",
+    "C": "#FFE61F",
+    "E": "#A00018",
+    "D": "#FF0025",
+    "R": "#450270",
+    "K": "#8400CD",
+    "H": "#FF3CFE",
+}
